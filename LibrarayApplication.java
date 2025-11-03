c39d040324be2a439a996139ae36b1b045b1c9d3
jaemoon <ljmm5721@naver.com>
2025-11-03 16:24:29 +0900

    .

--- LibrarayApplication.java
+++ LibrarayApplication.java
@@ -9,19 +9,18 @@ public class LibrarayApplication
 {
     private String name;
     
-    public Library(String name){
+    public LibrarayApplication(String name){
         this.name = name;
     }
     
     public void registerOneBorrower(String name, int stID){
         System.out.println("대출자 등록 완료:");
-        System.out.println("이름: " + name);
-        System.out.println("학생 ID: " + stID);
+        System.out.println("이름: " + this.name);
+        System.out.println("학생 ID: " + this.stID);
     }
     
     public void displayBookForLoan(){
-        System.out.println("대출 가능한 책 목록을 표시합니다.");
-        System.out.println("소프트웨어 분석설계");
+        System.out.println("-----------대출 가능한 책 목록-----------.");
     }
     
     public void lendOneBook(){
